0436da5d8c503d95db4ba1e86e18323f6822252b
Benny Tapiero <bntapiero@gmail.com>
2026-01-28 18:56:43 +0200

    Message décrivant les changements

--- Services/AuthService.cs
+++ Services/AuthService.cs
@@ -154,7 +154,7 @@ namespace PaieApi.Services
             {
                 public string id { get; set; }
                 public string email { get; set; }
-                public string verified_email { get; set; }
+                public bool verified_email { get; set; }
                 public string name { get; set; }
                 public string given_name { get; set; }
                 public string family_name { get; set; }
